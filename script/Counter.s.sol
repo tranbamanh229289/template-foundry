be9aad16a1330402c422e0c1c160b0f5394aefb9
Manh Tran Ba <manhtb@moonlab.asia>
2024-07-25 08:25:50 +0700

    update

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,11 +2,18 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
-    function setUp() public {}
+  address deployer;
+  function setUp() public {}
 
-    function run() public {
-        vm.broadcast();
-    }
+  function run() public {
+    uint256 privateKey = vm.envUint("PRIVATE_KEY");
+    address account = vm.addr(privateKey);
+    console.log("Account", account);
+    vm.startBroadcast(privateKey);
+    Counter counter = new Counter();
+    vm.stopBroadcast();
+  }
 }
